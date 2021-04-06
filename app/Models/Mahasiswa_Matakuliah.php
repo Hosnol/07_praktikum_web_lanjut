5433def530fb712c2bfbbd8b54564fd29df3334e
Hosnol Arifin <hosnolarifin220@gmail.com>
2021-04-06 18:47:21 +0700

    prak 9 - edit file model Mahasiswa matakuliah

--- app/Models/Mahasiswa_Matakuliah.php
+++ app/Models/Mahasiswa_Matakuliah.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Mahasiswa;
+use App\Models\Matakuliah;
 
 class Mahasiswa_Matakuliah extends Model
 {
     use HasFactory;
+    protected $table = "mahasiswa_matakuliah";
+    
+    public function mahasiswa()
+    {
+        return $this->belongsToMany(Mahasiswa::class);
+    }
+
+    public function matakuliah()
+    {
+        return $this->belongsToMany(Matakuliah::class);
+    }
 }
